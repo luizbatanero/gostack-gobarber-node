8105648b19c2c1764cddb19d319c3821ee9ebf10
Luiz Batanero <luizbatanero@gmail.com>
2019-06-25 22:27:37 -0300

    Send cancellation email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'e78381f4008407',
+    pass: '19497d79d67007',
+  },
+  default: {
+    from: 'GoBarber <noreply@gobarber.com>',
+  },
+};
